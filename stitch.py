c7c02b742e52cd7067294c2f6ad7512ff9a7d024
Alessio Belli <58253493+alessioBelli@users.noreply.github.com>
2023-07-21 22:35:24 +0200

    Update stitch.py
    Final video creation fixed

--- stitch.py
+++ stitch.py
@@ -188,18 +188,15 @@ class Stitcher:
 def convert_frames_to_video(pathIn, pathOut, fps, n_frames):
     print("Creating final video...")
     frame_array = []  
+    filename=pathIn + "frame_0.jpg"
+    img = cv.imread(filename)
+    height, width, _ = img.shape
+    size = (width,height)
+    out = cv.VideoWriter(pathOut,cv.VideoWriter_fourcc(*'DIVX'), fps, size)
     for i in tqdm(range(n_frames)):
         filename=pathIn + "frame_" + str(i)+".jpg"
-        #reading each files
-        img = cv.imread(filename)
-        height, width, layers = img.shape
-        size = (width,height)
-        #inserting the frames into an image array
-        frame_array.append(img)
-    out = cv.VideoWriter(pathOut,cv.VideoWriter_fourcc(*'DIVX'), fps, size)
-    for i in range(len(frame_array)):
-        # writing to a image array
-        out.write(frame_array[i])
+        frame = cv2.imread(filename)
+        out.write(frame)
     out.release()
 
 def stitch_images(stitcher, n_frames):
